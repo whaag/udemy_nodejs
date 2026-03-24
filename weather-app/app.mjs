d9aa45b03f48f06eeea3d9fb40f80dc29136ca5d
whaag <eduardo.a.athayde@gmail.com>
2026-03-24 18:50:09 +0000

    section-6 switched to open meteo because the other one was offline

--- weather-app/app.mjs
+++ weather-app/app.mjs
@@ -1,9 +1,5 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=Dublin&units=m`;
+const url = `https://api.open-meteo.com/v1/forecast?current=temperature_2m,precipitation_probability,apparent_temperature&latitude=53.3331&longitude=-6.2489`;
 
 fetch(url)
 .then((response) => response.json())
-.then((data) => console.log(data.current.temperature));
+.then((data) => console.log(`Temperature: ${data.current.temperature_2m}${data.current_units.temperature_2m}`));
