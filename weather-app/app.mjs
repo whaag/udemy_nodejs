8da1f5261ec16846a5bf4c3a55e933579b456c6f
whaag <eduardo.a.athayde@gmail.com>
2026-03-27 15:54:31 +0000

    section-6 add error handling

--- weather-app/app.mjs
+++ weather-app/app.mjs
@@ -1,5 +1,13 @@
 const url = `https://api.open-meteo.com/v1/forecast?current=temperature_2m,precipitation_probability,apparent_temperature&latitude=53.3331&longitude=-6.2489`;
 
-fetch(url)
-.then((response) => response.json())
-.then((data) => console.log(`Temperature: ${data.current.temperature_2m}${data.current_units.temperature_2m}`));
+(async () => {
+    try {
+        await fetch(url)
+            .then((response) => response.json())
+            .then((data) => console.log(`Temperature: ${data.current.temperature_2m}${data.current_units.temperature_2m}`));
+    } catch (error) {
+        console.error("Error");
+    }
+})();
+
+
