950c53658e4aeb0f1177242b00bab1eeb3d74549
Whaag <eduardo.a.athayde@gmail.com>
2026-07-07 21:42:20 +0100

    section-7 add static pages on web-server

--- web-server/src/app.mjs
+++ web-server/src/app.mjs
@@ -1,25 +1,12 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-// app.com
-app.get('', (request, response) => {
-    response.send('Express working in here');
-});
-
-// app.com/help
-app.get('/help', (request, response) => {
-    response.send('Here, have some help');
-});
-
-// app.com/about
-app.get('/about', (request, response) => {
-    response.send('Familly secret. All to do with herbs and spicies');
-});
+app.use(express.static("public"));
 
 // app.com/weather
-app.get('/weather', (request, response) => {
-    response.send(`Rain. It's Dublin, what did you think?`);
-})
+app.get("/weather", (request, response) => {
+  response.send(`Rain. It's Dublin, what did you think?`);
+});
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(3000, () => console.log("Server started on port 3000"));
