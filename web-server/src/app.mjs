ea907f45abaefbb3cce326ab74f3e08843eb1f94
Whaag <athayde.eduardo@proton.me>
2026-08-11 21:02:05 +0100

    section-7 template for content not found

--- web-server/src/app.mjs
+++ web-server/src/app.mjs
@@ -33,11 +33,29 @@ app.get("/help", (request, response) => {
     name: 'Whaag',
     message: 'Do not take candy from strangers'
   });
-})
+});
+
+// app.com/help/404
+app.get("/help/*path", (request, response) => {
+  response.render('error', {
+    title: 'Weather App - Error 404',
+    name: 'Whaag',
+    errorMessage: 'Article not found'
+  });
+});
 
 // app.com/weather
 app.get("/weather", (request, response) => {
   response.send(`Rain. It's Dublin, what did you think?`);
+});
+
+// default
+app.get("/*path", (request, response) => {
+  response.render('error', {
+    title: 'Weather App - Error 404',
+    name: 'Whaag',
+    errorMessage: 'Page not found'
+  });
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
